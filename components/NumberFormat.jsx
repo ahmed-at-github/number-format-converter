94d0be99f5614a2e88ea66a3439c3bda55ed0cec
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-04-27 19:54:28 +0600

    0.0.1 [Numberformat.jsx UI design made]

--- components/NumberFormat.jsx
+++ components/NumberFormat.jsx
@@ -1,19 +1,20 @@
-import "../src/numberformat-styles.css"
+import "./NumberFormat.css"
 
-function NumberFormat() {
+export default function NumberFormat() {
   return (
     <div className="application">
       <h1>Number Format Converter</h1>
       <div className="section">
         <div className="row">
-          <p>From</p>
+          <p>Number Format</p>
           <select value={0} onChange={0}>
             <option value="binary">Binary</option>
-            <option value="decimal">Decimal</option>
             <option value="octal">Octal</option>
+            <option value="decimal">Decimal</option>
             <option value="hexadecimal">Hexadecimal</option>
           </select>
         </div>
+
         <div className="row">
           <p>Enter {0} Number</p>
           <div>
@@ -21,51 +22,68 @@ function NumberFormat() {
             <button onClick={0}>Convert</button>
           </div>
         </div>
+
         <div className="row">
           <p>Integer Number</p>
           <input type="number" value={0} onChange={0} />
         </div>
+
         <div className="row">
           <p>Significant Number</p>
           <div>
             <input type="number" value={0} onChange={0} />
-            <select value={0} onChange={0}></select>
+            <select value={0} onChange={0}>
+              {Array}
+            </select>
           </div>
         </div>
+
         <div className="row">
           <p>Rounded Number</p>
           <div>
             <input type="number" value={0} onChange={0} />
-            <select value={0} onChange={0}></select>
+            <select value={0} onChange={0}>
+              {Array}
+            </select>
           </div>
         </div>
+
         <div className="row">
           <p>Fraction</p>
           <div>
             <input type="number" value={0} onChange={0} />
+            <p>&nbsp;/&nbsp;</p>
             <input type="number" value={0} onChange={0} />
             <input type="number" value={0} onChange={0} />
           </div>
         </div>
+
         <div className="row">
-          <p></p>
-          <input type="number" value={0} onChange={0} />
-        </div>{" "}
-        <div className="row">
-          <p></p>
-          <input type="number" value={0} onChange={0} />
-        </div>{" "}
-        <div className="row">
-          <p></p>
+          <p>
+            {0} of integer {0}
+          </p>
           <input type="number" value={0} onChange={0} />
         </div>
+
         <div className="row">
-          <p>In words of Integer {0}</p>
+          <p>
+            {0} of integer {0}
+          </p>
+          <input type="number" value={0} onChange={0} />
+        </div>
+
+        <div className="row">
+          <p>
+            {0} of integer {0}
+          </p>
+          <input type="number" value={0} onChange={0} />
+        </div>
+
+        <div className="row">
+          <p>In words</p>
           <input type="text" value={0} onChange={0} />
         </div>
       </div>
     </div>
-  );
+  )
 }
-
-export default NumberFormat;
