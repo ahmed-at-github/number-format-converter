97c0e0d3bf73d370eb679992c33cfb087444516f
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-04-27 20:09:12 +0600

    0.0.2 [Numberformat.jsx Logic added half]

--- components/NumberFormat.jsx
+++ components/NumberFormat.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
 import "./NumberFormat.css"
 
 export default function NumberFormat() {
+  //LOGIC
+  const [inputFormat, setInputFormat] = useState("decimal") //initial format
+  const [inputNumber, setInputNumber] = useState("") //initial Number (any format)
+  const [decimal, setDecimal] = useState("") //isWhichFormat
+  const [binary, setBinary] = useState("")
+  const [octal, setOctal] = useState("")
+  const [hexadecimal, setHexadecimal] = useState("")
+  const [integer, setInteger] = useState("") //Number in Integer only
+  const [significantNo, setSignificantNo] = useState("") //Number in significan format
+  const [significantNoIndex, setSignificantNoIndex] = useState("2") //??
+  const [roundDigit, setRoundDigit] = useState("") //Number in round
+  const [roundDigitIndex, setRoundDigitIndex] = useState("2") //??
+  const [numerator, setNumerator] = useState("") //Number in frac
+  const [denominator, setDenominator] = useState("") //??
+  const [inWord, setInWord] = useState("") //Number to word
+
+  //UI
   return (
     <div className="application">
       <h1>Number Format Converter</h1>
