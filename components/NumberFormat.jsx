d46528b032f0370784788eba77c41482e1f79d82
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-04-27 21:19:14 +0600

    0.0.3 [Numberformat.jsx Logic is built, but not coupled with UI]

--- components/NumberFormat.jsx
+++ components/NumberFormat.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import "./NumberFormat.css"
+import numberToWords from "number-to-words"
 
 export default function NumberFormat() {
   //LOGIC
-  const [inputFormat, setInputFormat] = useState("decimal") //initial format
-  const [inputNumber, setInputNumber] = useState("") //initial Number (any format)
-  const [decimal, setDecimal] = useState("") //isWhichFormat
+  const [inputFormat, setInputFormat] = useState("decimal") //initial-Given format
+  const [inputNumber, setInputNumber] = useState("") //initial-Given Number (any format)
+  const [decimal, setDecimal] = useState("") //conversion
   const [binary, setBinary] = useState("")
   const [octal, setOctal] = useState("")
   const [hexadecimal, setHexadecimal] = useState("")
@@ -17,6 +18,123 @@ export default function NumberFormat() {
   const [numerator, setNumerator] = useState("") //Number in frac
   const [denominator, setDenominator] = useState("") //??
   const [inWord, setInWord] = useState("") //Number to word
+
+  //EVENT TRIGGER-HANDLER
+  const handleConversion = () => {
+    let decimalValue
+
+    //converting all_format to decimal
+    switch (inputFormat) {
+      case "binary":
+        decimalValue = parseInt(inputNumber, 2)
+        break
+      case "octal":
+        decimalValue = parseInt(inputNumber, 8)
+        break
+      case "hexadecimal":
+        decimalValue = parseInt(inputNumber, 16)
+        break
+      default:
+        decimalValue = parseInt(inputNumber, 10)
+    }
+    if (inputFormat !== "decimal") setDecimal(decimalValue) //??
+
+    //Finding Integer value
+    setInteger(Math.floor(decimalValue))
+
+    //converting and setting format (decimal to other_format)
+    setBinary(Math.floor(decimalValue).toString(2))
+    setHexadecimal(Math.floor(decimalValue).toString(16).toUpperCase())
+    setOctal(Math.floor(decimalValue).toString(8))
+
+    //Setting number-to-word
+    if (decimalValue <= 1000000000000000)
+      setInWord(numberToWords.toWords(decimalValue))
+    else setInWord("Over Limit (Max-Limit : 1000000000000000")
+
+    //Round Number
+    if (inputFormat === "decimal")
+      setRoundDigit(
+        roundToKthInteger(
+          parseFloat(decimal, 10),
+          parseInt(roundDigitIndex, 10)
+        )
+      )
+    else
+      setRoundDigit(
+        roundToKthInteger(
+          parseFloat(decimalValue, 10),
+          parseInt(roundDigitIndex, 10)
+        )
+      )
+
+    //Fraction
+    if (
+      inputFormat === "decimal" &&
+      parseFloat(decimal, 10) - decimalValue !== 0
+    ) {
+      const result = floatToFraction(parseFloat(decimal, 10) - decimalValue)
+      setNumerator(result.Numerator)
+      setDenominator(result.denominator)
+    } else {
+      setNumerator(0)
+      setDenominator(0)
+    }
+
+    //Significant Number
+    if (inputFormat === "decimal")
+      setSignificantNo(
+        roundToSignificantDigits(
+          parseFloat(decimal, 10),
+          parseint(significantNoIndex, 10)
+        )
+      )
+    else
+      setSignificantNo(
+        roundToSignificantDigits(
+          parseFloat(decimalValue, 10),
+          parseInt(significantNoIndex, 10)
+        )
+      )
+  }
+
+  //OTHER LOGIC
+  function floatToFraction(number) {
+    const tolerance = 0.000001 //defines how close the fraction must be to the real number.
+    let numerator = 1
+    let denominator = 1
+    let error = number - numerator / denominator
+
+    while (Math.abs(error) > tolerance) {
+      if (error > 0) numerator++
+      else denominator++
+
+      error = number - numerator / denominator
+    }
+    return {
+      numerator,
+      denominator,
+    }
+  }
+
+  //to round a number to k precision
+  function roundToKthInteger(number, k) {
+    const multiplier = Math.pow(10, k)
+    return Math.round(number * multiplier) / multiplier
+  }
+
+  //rounds a number to a given number of significant digits (12345, 2) -> 12000
+  //High logic DONT UNDERSTAND
+  function roundToSignificantDigits(number, significantDigits) {
+    if (significantDigits <= 0) return 0
+    const multiplier = Math.pow(
+      10,
+      significantDigits - Math.floor(Math.log10(Math.abs(number))) - 1
+    )
+    const roundedNumber = Math.round(number * multiplier) / multiplier
+
+    return roundedNumber
+  }
 
   //UI
   return (
